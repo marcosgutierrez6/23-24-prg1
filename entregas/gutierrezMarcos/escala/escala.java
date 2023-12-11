3ff4882c2534af248453b95a81e294a1922ef711
marcosgutierrez6 <144948806+marcosgutierrez6@users.noreply.github.com>
2023-12-11 10:04:40 +0100

    Update escala.java

--- entregas/gutierrezMarcos/escala/escala.java
+++ entregas/gutierrezMarcos/escala/escala.java
@@ -3,30 +3,36 @@ public class escala {
 
     public static void main(String[] args) {
         String[] escalaMayor;
+        String[] acorde;
 
         escalaMayor = escalaMayor("Do");
         mostrarNotas(escalaMayor);
+        acorde = construirAcorde(escalaMayor);
+        mostrarNotas(acorde);
     }
 
+    private static String[] construirAcorde(String[] escala) {
+        String[] acorde = { escala[0], escala[2], escala[4] };
+        return acorde;
+    }
 
-    private static String[] escalaMayor(String nota){
-        String[] patrones = {"T", "T", "S", "T", "T", "T", "S"};
+    private static String[] escalaMayor(String nota) {
+        String[] patrones = { "T", "T", "S", "T", "T", "T", "S" };
         String[] escala = new String[8];
         int indiceNota = obtenerIndice(nota);
 
-        for (int i = 0;i < escala.length; i++){
+        for (int i = 0; i < escala.length; i++) {
             escala[i] = obtenerNota((indiceNota) % 12);
-            if (i < 7){
-                if (patrones[i].equals("T")){
+            if (i < 7) {
+                if (patrones[i].equals("T")) {
                     indiceNota += 2;
-                }else{
+                } else {
                     indiceNota++;
                 }
             }
         }
         return escala;
     }
-
 
     private static int obtenerIndice(String nota) {
         int i;
