0ab23f04c2531e4bb286abef85b8c86d1c4c3aec
Marcos Gutiérrez <144948806+marcosgutierrez6@users.noreply.github.com>
2026-03-05 12:55:14 +0100

    fix(format): mover atributos al principio y minimizar el alcance de las variables locales

--- entregas/gutierrezMarcos/Examen/ExamenParcialPRG1.java
+++ entregas/gutierrezMarcos/Examen/ExamenParcialPRG1.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 
 public class ExamenParcialPRG1 {
+    static int acumulacionConsumo = 0;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int consumoDia1 = 0;
@@ -10,22 +12,22 @@ public class ExamenParcialPRG1 {
         int consumoDia5 = 0;
         int consumoDia6 = 0;
         int consumoDia7 = 0;
-        int mediaConsumoSemanal = 0;
-        int plantaEnMantenimiento = 0;
-        int mantenimientoDiario = 0;
-        int rayoDiario = 0;
+        
         int columnaImpactada = 0;
+        
         if(rayo()){
             columnaImpactada = (int) (Math.random() * 6) + 1;
         }
 
         for (int i = 1; i < 8; i++) {
-            rayoDiario = 0;
-            mantenimientoDiario = 0;
+            int rayoDiario = 0;
+            int mantenimientoDiario = 0;
+            
             for (int j = 1; j <= 24; j++) {
-                System.out.println("               __/\\__\r\n" + //
-                        "  |    |    |  |####|  |    |    |  \r\n" + //
+                System.out.println("               __/\\__\r\n" + 
+                        "  |    |    |  |####|  |    |    |  \r\n" + 
                         "====================================");
+                
                 for (int k = 7; k >= 1; k--) {
                     if (rayoDiario < 1) {
                         if (columnaImpactada > 0) {
@@ -48,7 +50,7 @@ public class ExamenParcialPRG1 {
                     } else if (mantenimientoDiario < 1) {
                         if (mantenimiento()) {
                             mantenimientoDiario++;
-                            plantaEnMantenimiento = (int) (Math.random() * 7) + 1;
+                            int plantaEnMantenimiento = (int) (Math.random() * 7) + 1;
                             if (plantaEnMantenimiento == k) {
                                 System.out.println(":[#]::[#]::[#]:[    ]:[#]::[#]::[#]: - P" + k);
                                 k--;
@@ -57,18 +59,18 @@ public class ExamenParcialPRG1 {
                             System.out.println(":" + estadoVentana() + "::" + estadoVentana() + "::" + estadoVentana() + ":[    ]:"+ estadoVentana() + "::" + estadoVentana() + "::" + estadoVentana()+ ": - P"+ k);
                         }
                     } else {
-
                         System.out.println(":" + estadoVentana() + "::" + estadoVentana() + "::" + estadoVentana() + ":[    ]:"+ estadoVentana() + "::" + estadoVentana() + "::" + estadoVentana() + ": - P"+ k);
                     }
-
                 }
+                
                 columnaImpactada = 0;
-                System.out.println("------------------------------------\r\n" + //
-                        "     ==========================\r\n" + //
-                        "   ==============================\r\n" + //
+                System.out.println("------------------------------------\r\n" + 
+                        "     ==========================\r\n" + 
+                        "   ==============================\r\n" + 
                         " ==================================");
 
                 System.out.println("Dia " + i + " - " + j + ":00h Consumo hora: 22");
+                
                 if (i == 1) {
                     consumoDia1 = consumoDia1 + obtenerAcumulacionConsumo();
                     System.out.println("D" + i + ":" + " " + consumoDia1);
@@ -79,51 +81,40 @@ public class ExamenParcialPRG1 {
                     acumulacionConsumo = 0;
                 } else if (i == 3) {
                     consumoDia3 = consumoDia3 + obtenerAcumulacionConsumo();
-                    System.out.println(
-                            "D1: " + consumoDia1 + "| D2: " + consumoDia2 + "| D" + i + ":" + " " + consumoDia3);
+                    System.out.println("D1: " + consumoDia1 + "| D2: " + consumoDia2 + "| D" + i + ":" + " " + consumoDia3);
                     acumulacionConsumo = 0;
                 } else if (i == 4) {
                     consumoDia4 = consumoDia4 + obtenerAcumulacionConsumo();
-                    System.out.println("D1: " + consumoDia1 + "| D2: " + consumoDia2 + "| D3: " + consumoDia3 + "| D"
-                            + i + ":" + " " + consumoDia4);
+                    System.out.println("D1: " + consumoDia1 + "| D2: " + consumoDia2 + "| D3: " + consumoDia3 + "| D" + i + ":" + " " + consumoDia4);
                     acumulacionConsumo = 0;
                 } else if (i == 5) {
                     consumoDia5 = consumoDia5 + obtenerAcumulacionConsumo();
-                    System.out.println("D1: " + consumoDia1 + "| D2: " + consumoDia2 + "| D3: " + consumoDia3 + "| D4:"
-                            + consumoDia4 + " | D" + i + ":" + " " + consumoDia5);
+                    System.out.println("D1: " + consumoDia1 + "| D2: " + consumoDia2 + "| D3: " + consumoDia3 + "| D4:" + consumoDia4 + " | D" + i + ":" + " " + consumoDia5);
                     acumulacionConsumo = 0;
                 } else if (i == 6) {
                     consumoDia6 = consumoDia6 + obtenerAcumulacionConsumo();
-                    System.out.println("D1: " + consumoDia1 + "| D2: " + consumoDia2 + "| D3: " + consumoDia3 + "| D4:"
-                            + consumoDia4 + "| D5:" + consumoDia5 + " | D" + i + ":" + " " + consumoDia6);
+                    System.out.println("D1: " + consumoDia1 + "| D2: " + consumoDia2 + "| D3: " + consumoDia3 + "| D4:" + consumoDia4 + "| D5:" + consumoDia5 + " | D" + i + ":" + " " + consumoDia6);
                     acumulacionConsumo = 0;
                 } else if (i == 7) {
                     consumoDia7 = consumoDia7 + obtenerAcumulacionConsumo();
-                    System.out.println("D1: " + consumoDia1 + "| D2: " + consumoDia2 + "| D3: " + consumoDia3 + "| D4:"
-                            + consumoDia4 + "| D5:" + consumoDia5 + "| D6: " + consumoDia6 + " | D" + i + ":" + " "
-                            + consumoDia7);
+                    System.out.println("D1: " + consumoDia1 + "| D2: " + consumoDia2 + "| D3: " + consumoDia3 + "| D4:" + consumoDia4 + "| D5:" + consumoDia5 + "| D6: " + consumoDia6 + " | D" + i + ":" + " " + consumoDia7);
                     acumulacionConsumo = 0;
                 }
-                scanner.nextInt();
+                
+                scanner.nextLine(); 
             }
             rayoDiario++;
         }
+
+        int mediaConsumoSemanal = (consumoDia1 + consumoDia2 + consumoDia3 + consumoDia4 + consumoDia5 + consumoDia6 + consumoDia7) / 7;
         
-        mediaConsumoSemanal = (consumoDia1 + consumoDia2 + consumoDia3 + consumoDia4 + consumoDia5 + consumoDia6
-                + consumoDia7) / 7;
         System.out.println("\nCONSUMOS: D1: " + consumoDia1 + " | D2: " + consumoDia2 + " | D3: " + consumoDia3
-                + " | D4: " + consumoDia4 + " | D5: " + consumoDia5 + " | D6: " + consumoDia6 + " | D7: " + consumoDia7
-                + " | \r\n" + //
-                "\r\n" + //
+                + " | D4: " + consumoDia4 + " | D5: " + consumoDia5 + " | D6: " + consumoDia6 + " | D7: " + consumoDia7 + " | \r\n\r\n" + 
                 "Media de consumo semanal: " + mediaConsumoSemanal);
-
+        
+        scanner.close();
     }
-
-    static int acumulacionConsumo = 0;
-
-    static int obtenerAcumulacionConsumo() {
-        return acumulacionConsumo;
-    }
+    static int obtenerAcumulacionConsumo() { return acumulacionConsumo; }
 
     static String estadoVentana() {
         String ventana = "";
