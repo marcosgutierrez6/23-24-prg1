805f685336b03fbc47b0ad4692268ad8932300c4
Marcos Gutiérrez <144948806+marcosgutierrez6@users.noreply.github.com>
2026-03-08 10:59:25 +0100

    Fix: Consistencia en las variables

--- entregas/gutierrezMarcos/reto-004/Decostruccion.java
+++ entregas/gutierrezMarcos/reto-004/Decostruccion.java
@@ -1,24 +1,27 @@
 import java.util.Scanner;
 
 public class Decostruccion {
+    
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Ingrese el primer numero de tres cifras: ");
-        int primerNumero = scanner.nextInt();
-
-        System.out.print("Ingrese el segundo numero de tres cifras: ");
-        int segundoNumero = scanner.nextInt();
-
-        System.out.print("Ingrese el tercer numero de tres cifras: ");
-        int tercerNumero = scanner.nextInt();
+        
+        int primerNumero = pedirNumero("primer");
+        int segundoNumero = pedirNumero("segundo");
+        int tercerNumero = pedirNumero("tercer");
 
         int primeraCifra = primerNumero / 100;
         int segundaCifra = (segundoNumero / 10) % 10;
         int terceraCifra = tercerNumero % 10;
 
-        int nuevoNumero = (cifra1 * 100) + (cifra2 * 10) + cifra3;
+        int nuevoNumero = (primeraCifra * 100) + (segundaCifra * 10) + terceraCifra;
 
         System.out.println("El nuevo numero formado es: " + nuevoNumero);
+        
+        scanner.close();
+    }
+    private static int pedirNumero(String orden) {
+        System.out.print("Ingrese el " + orden + " numero de tres cifras: ");
+        return scanner.nextInt();
     }
 }
